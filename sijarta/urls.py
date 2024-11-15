eabe311bd234d442303101e699ac0c3017268060
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-11-15 22:35:30 +0700

    set routing

--- sijarta/urls.py
+++ sijarta/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('kuning/', include('kuning.urls')),
+    path('hijau/', include('hijau.urls')),
+    path('biru/', include('biru.urls')),
+    path('merah/', include('merah.urls')),
 ]
