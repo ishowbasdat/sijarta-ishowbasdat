d38dd7d3447b278f200f64e13d8ce1cee788cfa6
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-11-15 22:53:42 +0700

    change app name

---
+++ biru/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'biru'
+
+urlpatterns = [
+    path('', views.show_main, name='main'),
+]
--- sijarta/urls.py
+++ sijarta/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('testi_diskon_pbvoucher.urls')),
+    path('', include('biru.urls')),
 ]
