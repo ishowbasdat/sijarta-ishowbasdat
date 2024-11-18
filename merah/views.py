5626b3aece2715687a88f178847fad3f46ac30fa
Scallss <pascalhafidz2005@gmail.com>
2024-11-18 12:56:55 +0700

    feat: better page design

--- merah/views.py
+++ merah/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 def mypay_info(request):
     return render(request, 'mypay_info.html')
 
-def mypay_transaction(request):
-    # Menentukan tipe user (misalnya berdasarkan role atau langsung dari request.user)
-    user_type = 'pengguna'  # Anda bisa sesuaikan ini dengan logika berdasarkan user yang login
-    
-    context = {
-        'user_type': user_type,
-    }
-    
-    return render(request, 'mypay_transaction.html', context)
+def mypay_transaction(request):    
+    return render(request, 'mypay_transaction.html')
 
 def pekerjaan_jasa(request):
     return render(request, 'pekerjaan_jasa.html')
