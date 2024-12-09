3038340fa02bea88010572d1f69112ac9c66b146
Muhammad Afwan Hafizh <158464163+mir4na@users.noreply.github.com>
2024-12-09 20:56:22 +0700

    Update urls.py

--- sijarta/urls.py
+++ sijarta/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('kuning/', include('kuning.urls')),
-    #path('hijau/', include('hijau.urls')),
-    #path('biru/', include('biru.urls')),
-    #path('merah/', include('merah.urls')),
+    path('hijau/', include('hijau.urls')),
+    path('biru/', include('biru.urls')),
+    path('merah/', include('merah.urls')),
 ]
