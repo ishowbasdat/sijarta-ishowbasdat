f98693ea9bfa8c1d96c23463417aad6c8b1eb728
papaChick <raphaelheryanto270605@gmail.com>
2024-12-09 22:37:10 +0700

    deployment and other stuff

--- sijarta/settings.py
+++ sijarta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6$0^=54dp$!i3zcn1ujghixcs0145j0lukg5bb*sb)1)!34$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG' , True)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,11 +80,11 @@ WSGI_APPLICATION = 'sijarta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sijarta',
-        'USER': 'sijarta',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('PGDATABASE', 'sijarta'), 
+        'USER': os.getenv('PGUSER', 'sijarta'),    
+        'PASSWORD': os.getenv('PGPASSWORD', 'mypassword'),  
+        'HOST': os.getenv('PGHOST', 'localhost'), 
+        'PORT': os.getenv('PGPOST', '5432'),         
     }
 }
 
@@ -122,13 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static' 
-    ]
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
