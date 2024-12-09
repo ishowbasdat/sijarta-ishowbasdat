3456ffd6ef363ec8c387f754d9b3671e56bc4a23
Muhammad Afwan Hafizh <158464163+mir4na@users.noreply.github.com>
2024-12-09 20:55:56 +0700

    Create urls.py

---
+++ hijau/urls.py
@@ -0,0 +1,13 @@
+from .views import homepage, subkategori, get_metode_pembayaran, create_order, pesanan, cancel_pesanan
+from django.urls import path
+
+app_name = 'hijau'
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('subkategori/<str:id>', subkategori, name='subkategori'),
+    path('api/get-metode-pembayaran/', get_metode_pembayaran, name='get_metode_pembayaran'),
+    path('api/create-order/', create_order, name='create_order'),
+    path('pesanan/', pesanan, name='pesanan'),
+    path('cancel-pesanan/<str:id>', cancel_pesanan, name='cancel_pesanan')
+]
