a302500200c9b4f53d53cb63a3ec6f91b6563f32
papaChick <raphaelheryanto270605@gmail.com>
2024-12-09 22:47:29 +0700

    fix setting.py CSRF & psql password

--- sijarta/settings.py
+++ sijarta/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['*']
